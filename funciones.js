4a3068613df57ec39b56b13fe1351fb19ce86dc7
Nestor Omar Diaz <nestordiaz11@gmail.com>
2023-01-31 21:51:48 -0300

    Se crearon funciones de javascript

--- funciones.js
+++ funciones.js
@@ -1,4 +1,15 @@
 
-//document.getElementById('boton_datos').onclick = function () {
-//    document.getElementById('datos1').innerHTML = "Esto es una prueba de Javascript"
-//}
+document.getElementById("boton_datos").addEventListener("click", mostrar_datos);
+
+function mostrar_datos() {
+    document.getElementById("datos1").innerHTML = `Email: anthonyadams@example.com   &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp     &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp Teléfono: (766) 356-9721  <br>
+    <br>
+    Dirección: 1182 Mcclellan Rd., Michigan, E.E.U.U      &nbsp &nbsp &nbsp  &nbsp &nbsp   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Fecha de Nacimiento: 08/01/1973`};
+
+
+
+document.getElementById("boton_enviar").addEventListener("click", ver_consola);
+
+function ver_consola() {
+    window.alert("Los datos estan siendo enviados a la consola");
+};
